d0cfaabc37b07a68a076d869daf0185729eaef7f
Alejandro Resendez <al.resendez.gro@gmailcom>
2021-05-28 19:45:29 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+*/
+
+using System.IO.Ports;
+using System;
+
+namespace SerialSequencer
+{
+    class Program
+    {
+        static SerialPort ComPort;
+        static void Main(string[] args)
+        {
+            string cmd = "none";
+            string param = "none";
+            ComPort = new SerialPort();
+
+            // Set the ComPort Settings
+            ComPort.PortName = "COM3";
+            ComPort.BaudRate = 19200;
+            ComPort.Parity = Parity.None;
+            ComPort.DataBits = 8;
+            ComPort.StopBits = StopBits.One;
+
+            ComPort.ReadTimeout = 500;
+            ComPort.WriteTimeout = 500;
+
+            ComPort.Open();
+
+            while (cmd.Equals("exit") != true)
+            {
+                Console.WriteLine("Enter Command: >");
+                cmd = Console.ReadLine();
+                
+                switch (cmd)
+                {
+                    case "send_single":
+                        Console.WriteLine("Data to Send: >");
+                        param = Console.ReadLine();
+                        
+                        ComPort.WriteLine(param);
+                        break;
+                    case "send_sequence":
+                        Console.WriteLine("Command sequence: >");
+                        param = Console.ReadLine();
+                        send_command_seq(param);
+                        break;
+                    case "show_ports":
+                        PrintAvailableCOMs();
+                        break;
+
+                    case "arrows":
+
+                    case "exit":
+                        Console.WriteLine("Exit");
+                        break;
+
+                }
+            }
+
+            ComPort.Close();
+
+        }
+
+
+        public static void PrintAvailableCOMs()
+        {            
+            Console.WriteLine("Available Ports:");
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                Console.WriteLine("   {0}", s);
+            }
+        }
+
+        public static void send_command_seq(string filename)
+        {
+
+        }
+    }
+}
